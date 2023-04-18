751317b450e92ff0ebd9950889e79b7464b8b191
Willem Aaron <iamwillempacardo@gmail.com>
2023-04-19 01:06:29 +0800

    total revamp
    total revamp

--- script.js
+++ script.js
@@ -1,90 +1,46 @@
-// get the form and task list elements
-const addTaskForm = document.querySelector('#add-task-form');
-const taskList = document.querySelector('#task-list');
-
-// initialize the task array
-let tasks = [];
-
-// add event listener for form submission
-addTaskForm.addEventListener('submit', (event) => {
-  event.preventDefault(); // prevent form submission
-  const taskNameInput = addTaskForm.elements['task-name'];
-  const taskName = taskNameInput.value.trim();
-  if (taskName) {
-    addTask(taskName); // add task to list
-    taskNameInput.value = ''; // clear input field
-    taskNameInput.focus(); // focus on input field
+function displayCurrentTime() {
+    const currentTime = new Date();
+    const currentHour = currentTime.getHours();
+    const currentMinute = currentTime.getMinutes();
+    const currentSecond = currentTime.getSeconds();
+    const formattedTime = `${currentHour}:${currentMinute}:${currentSecond}`;
+    const timeDiv = document.querySelector(".current-time");
+    timeDiv.textContent = formattedTime;
   }
-});
-
-// function to add task to list
-function addTask(taskName) {
-  const task = {
-    id: Date.now(),
-    name: taskName,
-    completed: false
-  };
-  tasks.push(task); // add task to array
-  displayTask(task); // display task in list
-}
-
-// function to display task in list
-function displayTask(task) {
-  const taskItem = document.createElement('li');
-  taskItem.dataset.id = task.id;
-  taskItem.innerHTML = `
-    <label>
-      <input type="checkbox" class="checkbox"${task.completed ? ' checked' : ''}>
-      <span class="task-name">${task.name}</span>
-    </label>
-    <button class="delete-button">Delete</button>
-  `;
-  const deleteButton = taskItem.querySelector('.delete-button');
-  const checkbox = taskItem.querySelector('.checkbox');
-  checkbox.addEventListener('change', () => {
-    updateTaskCompleted(task.id, checkbox.checked);
-  });
-  deleteButton.addEventListener('click', () => {
-    deleteTask(task.id);
-  });
-  taskList.appendChild(taskItem);
-}
-
-// function to update task completed status
-function updateTaskCompleted(taskId, completed) {
-  const task = tasks.find((task) => task.id === taskId);
-  if (task) {
-    task.completed = completed;
-    const taskItem = taskList.querySelector(`[data-id="${task.id}"]`);
-    const checkbox = taskItem.querySelector('.checkbox');
-    if (completed) {
-      checkbox.classList.add('checked');
-    } else {
-      checkbox.classList.remove('checked');
+  
+  function addTask() {
+    const taskInput = document.getElementById("taskInput");
+    const taskList = document.getElementById("taskList");
+    const taskText = taskInput.value;
+    if (taskText.trim() !== "") {
+      const taskDiv = document.createElement("div");
+      taskDiv.classList.add("task");
+      const taskTextSpan = document.createElement("span");
+      taskTextSpan.textContent = taskText;
+      const deleteButton = document.createElement("button");
+      deleteButton.textContent = "Delete";
+      deleteButton.addEventListener("click", function() {
+        taskDiv.remove();
+      });
+      taskDiv.appendChild(taskTextSpan);
+      taskDiv.appendChild(deleteButton);
+      taskList.appendChild(taskDiv);
+      taskInput.value = "";
     }
   }
-}
+  document.addEventListener("keydown", function(event) {
+    if (event.key === "Enter") {
+      addTask();
+    }
+  });
 
-// function to delete task from list
-function deleteTask(taskId) {
-  tasks = tasks.filter((task) => task.id !== taskId);
-  const taskItem = taskList.querySelector(`[data-id="${taskId}"]`);
-  taskItem.remove();
-}
+  function clearTasks() {
+    const taskList = document.getElementById("taskList");
+    taskList.innerHTML = "";
+  }
 
-function updateTime() {
-  const date = new Date();
-  const hours = date.getHours();
-  const minutes = date.getMinutes();
-  const seconds = date.getSeconds();
-  const hoursString = hours.toString().padStart(2, '0');
-  const minutesString = minutes.toString().padStart(2, '0');
-  const secondsString = seconds.toString().padStart(2, '0');
-  const timeString = `${hoursString}:${minutesString}:${secondsString}`;
-  document.querySelector('#hours').textContent = hoursString;
-  document.querySelector('#minutes').textContent = minutesString;
-  document.querySelector('#seconds').textContent = secondsString;
-}
+const clearButton = document.getElementById("clearButton");
+clearButton.addEventListener("click", clearTasks);
 
-updateTime(); // update immediately
-setInterval(updateTime, 1000); // update every second
+  setInterval(displayCurrentTime, 1000);
+  
